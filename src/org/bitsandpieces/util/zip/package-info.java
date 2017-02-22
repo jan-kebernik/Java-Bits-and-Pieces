06d11795e98bbca8ca69693f61d32702a369d24f
Jan Kebernik <jan.kebernik@gmail.com>
2017-02-22 14:44:49 +0100

    Create package-info.java

---
+++ src/org/bitsandpieces/util/zip/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * This package provides a pure-Java Inflater and related Checksums.
+ */
+package org.bitsandpieces.util.zip;
