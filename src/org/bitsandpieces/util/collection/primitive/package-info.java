9871fee7c7e4aacbda2c077eb54d41c3f6b8a744
Jan Kebernik <jan.kebernik@gmail.com>
2017-02-22 14:55:43 +0100

    Create package-info.java

---
+++ src/org/bitsandpieces/util/collection/primitive/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Provides a bridge between primitive-based collections and the JDK collections
+ * framework.
+ */
+package org.bitsandpieces.util.collection.primitive;
